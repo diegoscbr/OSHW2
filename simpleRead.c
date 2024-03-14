b8d52ff963b3cdb9856114327eb08171af4ebd11
Diego Escobar <89594898+diegoscbr@users.noreply.github.com>
2024-03-14 15:10:03 +0000

    simpleRead with getLine

--- simpleRead.c
+++ simpleRead.c
@@ -6,29 +6,32 @@
 
 int main(int argc, char *argv[]) {
     FILE *file;
-    char line[256];
-    int lineNumber = 0;
-    srand(time(NULL));
-
+    int lineNumber = 0; // Line number
+    srand(time(NULL));// Seed random number generator
     // Open the file
     file = fopen(argv[1], "r");
     if (file == NULL) {
         printf("Unable to open the file.\n");
         return 1;
     }
+        // Declare a pointer to a char and a size_t for getline
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
     // Read and print each line
-    while (fgets(line, sizeof(line), file)) {
+    while ((read = getline(&line, &len, file)) != -1) {
         lineNumber++;
-        //printf("%s", line);
-            int waitTime = rand() % 4; // Wait for up to 4 seconds
-            sleep(waitTime);
-            printf("Line number: %d, Characters: %ld, Process ID: %d\n", lineNumber, strlen(line) - 1, getpid());
 
+        int waitTime = rand() % 4; // Wait for up to 4 seconds
+        sleep(waitTime);
+        printf("Line number: %d, Characters: %ld, Process ID: %d\n", lineNumber, read - 1, getpid());
     }
+
+    // Free the line buffer
+    free(line);
 
     // Close the file
     fclose(file);
-
     return 0;
 }
