eacc690b0500907cc8d6858310f59bd518b536f8
Diego Escobar <89594898+diegoscbr@users.noreply.github.com>
2024-03-18 23:31:46 +0000

    Now I have an interpreter that stores an array dynamically

--- pseudoShell/gush.c
+++ pseudoShell/gush.c
@@ -8,6 +8,7 @@
 
 void gush_loop();
 char* readLine();
+char** divideLine(char* line); //parse the line into tokens and return an array of the tokens
 
 
 int main(int argc, char* argv[]){
@@ -18,24 +19,73 @@ int main(int argc, char* argv[]){
 
 void gush_loop(){
     
-    char* line;
+    char* line; //line entered by user
+
+    char** argsArr; //array of tokens
+
     int EXIT_FLG = 0;
     do{
         printf("gush>");
+        //initially reado of line
         line = readLine();
-        if(strcmp(line,"exit") == 0){
-            EXIT_FLG = 1;
-        }
+        if(strcmp(line,"exit") == 0){EXIT_FLG = 1;} //check if user wants to exit
+        argsArr = divideLine(line); //parse the line into tokens array
+        printf("args[0]: %s\n", args[0]);
+        printf("args[1]: %s\n", args[1]);
         free(line);
+
         
     } while (EXIT_FLG == 0);
 }
-
+/*****************************/
+/*****************************/
+/*SIMPLE READ INITIALLY*/
 char* readLine() {
     char* line = NULL;
     size_t bufsize = 0;
-    getline(&line, &bufsize, stdin);
+    getline(&line, &bufsize, stdin); //getline will allocate memory for line
     line[strcspn(line, "\n")] = 0; //removes the newline character from the end of the string
     return line;
 }
+/*****************************/
+/*****************************/
+/*PARSE THE LINE:
+ ** into commands and arguments to be read*/
+char** divideLine(char* line){
+    int capacity = 10;
+    int size = 0;
+
+    char** tokArr = malloc(capacity * sizeof(char*));
+    if (tokArr == NULL){
+        fprintf(stderr, "allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    char* tokArrElement = strtok(line, " ");//get the first token
+
+    while(tokArrElement != NULL){
+        if(size >= capacity){
+            capacity *= 2;
+            tokArr = realloc(tokArr, capacity * sizeof(char*));
+        if (tokArr == NULL){
+            fprintf(stderr, "allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    tokArr[size] = tokArrElement;
+    size++;
+
+    tokArrElement = strtok(NULL, " ");
+    }
+    if(size >= capacity){
+        capacity *= 2;
+        tokArr = realloc(tokArr, (capacity+1) * sizeof(char*));
+        if (tokArr == NULL){
+            fprintf(stderr, "allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    tokArr[size] = NULL;
+    return tokArr;  
+}
+
 
