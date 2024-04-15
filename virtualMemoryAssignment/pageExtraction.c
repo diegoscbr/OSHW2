d9a67a0346b3a4b2916b7fe583a59076f321b3c1
Diego Escobar <89594898+diegoscbr@users.noreply.github.com>
2024-04-15 04:41:05 +0000

    getting somewhere

--- virtualMemoryAssignment/pageExtraction.c
+++ virtualMemoryAssignment/pageExtraction.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <fcntl.h>
-int get_offset(int address, int pageSize){
-    return address & (pageSize - 1);
-}
-int main(int argc, char * argv){
 
+#define TLB_SIZE 16
+#define FRAME_SIZE 256 //8 bits
+#define NUM_FRAMES 256
+#define PAGE_SIZE 256 //2^8 = 8 bits
+#define PAGE_TABLE_ENTRIES 256
+#define MEMORY_SIZE FRAME_SIZE * NUM_FRAMES
+
+#define OFFSET_BITS 8 //8 bits
+#define PAGE_NUMBER_BITS 8 //8 bits
+
+void getPageNumberAndOffset(int virtualAddress, int *pageNumber, int *offset) {
+    // Mask to extract the page number (VPN) - high-order 8 bits
+    *pageNumber = (virtualAddress >> 8) & 0xFF;
+
+    // Mask to extract the offset - low-order 8 bits
+    *offset = virtualAddress & 0xFF;
+}
+
+int main() {
+    // Iterate over integers from 0 to 65535 (2^16 - 1)
+    for (int i = 0; i <= 65535; ++i) {
+        int pageNumber, offset;
+        getPageNumberAndOffset(i, &pageNumber, &offset);
+        printf("Virtual Address: %5d, Page Number: %3d, Offset: %3d\n", i, pageNumber, offset);
+    }
 
     return 0;
 }
