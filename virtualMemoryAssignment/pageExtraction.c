300f8357dee5a6e056fdc12bf2940a83f32d8f4d
Diego Escobar <89594898+diegoscbr@users.noreply.github.com>
2024-04-16 01:30:54 +0000

    part 1

--- virtualMemoryAssignment/pageExtraction.c
+++ virtualMemoryAssignment/pageExtraction.c
@@ -2,19 +2,74 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#define TLB_SIZE 16
-#define FRAME_SIZE 256 //8 bits
-#define NUM_FRAMES 256
-#define PAGE_SIZE 256 //2^8 = 8 bits
-#define PAGE_TABLE_ENTRIES 256
-#define MEMORY_SIZE FRAME_SIZE * NUM_FRAMES
+#define PAGES 256
+#define PAGE_SIZE 256
+#define OFFSET_BITS 8
+#define OFFSET_MASK 255
+#define MEMO_SIZE PAGES * PAGE_SIZE
+#define BUF_SIZE 10
 
-#define OFFSET_BITS 8 //8 bits
-#define PAGE_NUMBER_BITS 8 //8 bits
+int pagetable[PAGES];
+signed char main_Memo[MEMO_SIZE];
+signed char *backing_ptr;
 
-int* pagetable[PAGE_TABLE_ENTRIES];
-int tlb[TLB_SIZE];
+int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        printf("USAGE: <./a.out> <input file>\n");
+        exit(0);
+    }
+
+    for (int i = 0; i < PAGES; i++) {
+        pagetable[i] = -1;
+    }
+
+    const char *file_name = "BACKING_STORE.bin";
+    const char *input_file = argv[1];
+    const char *output_file = "output.txt";
+    int backing_ptr_fd = open(file_name, O_RDONLY);
+    backing_ptr = mmap(0, MEMO_SIZE, PROT_READ, MAP_PRIVATE, backing_ptr_fd, 0);
+    FILE *input_fp = fopen(input_file, "r");
+    FILE *output_fp = fopen(output_file, "w");
+    char buf[BUF_SIZE];
+    unsigned char freePage = 0;
+    int total_addr = 0, pageFault = 0;
+
+    while (fgets(buf, BUF_SIZE, input_fp) != NULL) {
+        int logical_addr = atoi(buf);
+        int offset = logical_addr & OFFSET_MASK;
+        int logical = (logical_addr >> OFFSET_BITS);
+        int physical = pagetable[logical];
+
+        total_addr++;
+
+        if (physical == -1) { // Page Fault
+            pageFault++;
+
+            physical = freePage;
+            freePage++;
+
+            memcpy(main_Memo + physical * PAGE_SIZE, backing_ptr + logical * PAGE_SIZE, PAGE_SIZE);
+
+            pagetable[logical] = physical;
+        }
+
+        int physical_addr = (physical << OFFSET_BITS) | offset;
+        signed char value = main_Memo[physical * PAGE_SIZE + offset];
+
+        fprintf(output_fp, "Logical address: %d Physical address: %d Value: %d\n", logical_addr, physical_addr, value);
+    }
+
+    printf("Number of Translated Addresses = %d\n", total_addr);
+    printf("Page faults = %d\n", pageFault);
+    printf("Page Fault Rate = %.1f %%\n", ((float)pageFault / total_addr) * 100);
+
+    return 0;
+}
+
 
 int getPageNuber(int virtualAddress) {
     return (virtualAddress >> 8) & 0xFF;
@@ -28,14 +83,4 @@ int *setPageNumberAndOffset(int virtualAddress) {
     pageAndOffset[1] = getOffset(virtualAddress);
     return pageAndOffset;
 }
-int testAddress[] = {1, 256, 512, 32769, 128, 65534, 33153};
-int main() {
 
-    int* dataStore[7];
-    for (int i = 0; i <= 6; i++) {
-        dataStore[i] = setPageNumberAndOffset(testAddress[i]);
-        printf("Virtual Address: %5d, Page Number: %3d, Offset: %3d\n", testAddress[i], dataStore[i][0], dataStore[i][1]);
-    }
-
-    return 0;
-}
